2fb94330f88ddf99ed4a641ca35c2b2e7a9ab0e9
Eya soyed <eyaso@LAPTOP-NK71BM2P.swayedaya@gmail.com>
2024-04-30 11:26:21 +0100

    Querying for a Single Entity for a

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\VinylMix;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MixController extends AbstractController
 {
@@ -16,17 +17,32 @@ class MixController extends AbstractController
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
         $mix->setDescription('A pure mix of drummers turned singers!');
-        $mix->setGenre('pop');
+        $genres = ['pop', 'rock'];
+        $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
-        $genres = ['pop','rock'];
-        $mix->setGenre($genres[array_rand(($genres))]);
+
         $entityManager->persist($mix);
         $entityManager->flush();
+
         return new Response(sprintf(
             'Mix %d is %d tracks of pure 80\'s heaven',
             $mix->getId(),
             $mix->getTrackCount()
         ));
+    }
+
+    #[Route('/mix/{id}', name: 'app_mix_show')]
+    public function show($id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+
+        if (!$mix) {
+            throw $this->createNotFoundException('Mix not found');
+        }
+
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
